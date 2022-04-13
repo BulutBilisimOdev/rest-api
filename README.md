befa8779d62745945540dee885bbb6de778bbd7f
Salih Gumus <sudanmerinosu@gmail.com>
2022-04-13 23:18:48 +0300

    [feature][Salih] add get by id && update routes

--- README.md
+++ README.md
@@ -15,10 +15,20 @@ This is basic rest api for note taking.
 
 ```json
 //Example post body
-"note":"test123"
+{
+	"note": "test123"
+}
 ```
 
 - [x] GET /get | This route gets all the notes
-- [ ] GET /get/:id | This route gets spesific note by id
-- [ ] PATCH /update/:id | This route updates given note by id
+- [x] GET /get/:id | This route gets spesific note by id
+- [x] PATCH /update/:id | This route updates given note by id
+
+```json
+//Example update body
+{
+	"note": "updated note"
+}
+```
+
 - [ ] DELETE /delete/:id | This route deletes given note with id
--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,15 @@
 const { v4: uuidv4 } = require('uuid');
 
-let notes = [];
+let notes = [
+	{
+		id: '3bed2d48-7faa-428a-8990-866a446ac267',
+		note: 'test1',
+	},
+	{
+		id: '9832b125-ab7d-4636-838f-41c28da8b6bc',
+		note: 'test2',
+	},
+];
 
 module.exports = {
 	createNote: async (req, res) => {
@@ -16,7 +25,21 @@ module.exports = {
 	getNotes: async (_, res) => {
 		return res.status(200).json(notes);
 	},
-	getNoteById: async (req, res) => {},
-	updateNote: async (req, res) => {},
+	getNoteById: async (req, res) => {
+		const { id } = req.params;
+
+		const myNote = notes.find((note) => note.id === id);
+
+		return res.status(200).json(myNote);
+	},
+	updateNote: async (req, res) => {
+		const { id } = req.params;
+		const { note } = req.body;
+
+		const foundIndex = notes.findIndex((x) => x.id == id);
+
+		notes[foundIndex].note = note;
+		return res.status(200).json(notes[foundIndex]);
+	},
 	deleteNote: async (req, res) => {},
 };
